fb30172bec9bad5eca4cc840c6990dc0f3a9db71
Charles Collas <collas.charles@gmail.com>
2015-01-13 15:51:34 +0100

    Static files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
 var port = process.env.PORT || 5000;
 
@@ -11,11 +12,42 @@ var MyAPI = function(apiKey, secretKey) {
 };
 
 // Load of the index.html file (client side)
-var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
+var server = http.createServer(function (request, response) {
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+        
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+    
+    path.exists(filePath, function(exists) {
+    
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
     });
+    
 });
 
 // Socket.io's loading
